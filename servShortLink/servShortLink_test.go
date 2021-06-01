bc562926cd16cfe5af64b3b63d9d62a678040f49
0xBECEDA <sakharovasonya1@gmail.com>
2021-06-01 17:13:52 +0300

    all test

--- servShortLink/servShortLink_test.go
+++ servShortLink/servShortLink_test.go
@@ -23,11 +23,40 @@ func redirectGoogleCom( t *testing.T ) {
 
 	if status := rr.Code; status != http.StatusSeeOther {
 		t.Errorf("Возвращен неверный статус: получил %v ожидал %v",
-			status, http.StatusOK)
+			status, http.StatusSeeOther)
 	}
 }
 
-// проверяет получение короткой ссылки для google.com
+// проверяем счетчик посещений по короткой ссылке
+func getCntGoogleCom( t *testing.T, expected string ) {
+	req, err := http.NewRequest("GET", "/get_link_statistiсs/rGu2", nil)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(getLinkStatistiсs)
+
+	handler.ServeHTTP(rr, req)
+
+	body, err := io.ReadAll(rr.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := string(body)
+
+	if expected != got {
+		t.Errorf("ожидал: %v получил: %v",
+			expected, got)
+	}
+}
+
+// проверяет:
+// - получение короткой ссылки для google.com
+// - состояние счетчика посещений сразу после получения короткой ссылки (должно быть 0)
+// - перенаправление с короткой ссылки на google.com
+// - состояние счетчика посещений после перенаправления (должно быть 1 посещение)
 func TestGoogleCom(t *testing.T) {
 	req, err := http.NewRequest("GET", "/reg_new_link/https://www.google.com", nil)
 	if err != nil {
@@ -58,11 +87,13 @@ func TestGoogleCom(t *testing.T) {
 				expected, got)
 		}
 
+		getCntGoogleCom(t, "Адрес rGu2 посещали 0 раз \n")
 		redirectGoogleCom(t)
+		getCntGoogleCom(t, "Адрес rGu2 посещали 1 раз \n")
 	}
 }
 
-// проверяет возвращаемую ошибку, есть дать пустой урл
+// проверяет возвращаемую ошибку, есть дать пустой длинный урл
 func TestEmptyUrl(t *testing.T) {
 	req, err := http.NewRequest("GET", "/reg_new_link/", nil)
 	if err != nil {
