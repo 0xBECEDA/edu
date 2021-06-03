d61930267d8359f882b385d1757b4aeb7e58770a
0xBECEDA <sakharovasonya1@gmail.com>
2021-06-03 04:15:08 +0300

    doc file

--- servShortLink/servShortLink.go
+++ servShortLink/servShortLink.go
@@ -6,7 +6,7 @@ import
     "fmt"
     "net/http"
     "encoding/base64"
-	"net/url"
+    "net/url"
 )
 
 const hashLength = 4
@@ -17,25 +17,8 @@ type fullUrl struct {
     Url string
 }
 
-type getHashFromRequestError struct {
-	s string
-}
-
-func (e *getHashFromRequestError) Error() string {
-	return e.s
-}
-
 // таблица ссылок
 var tableOfLinks = make( map[string]fullUrl )
-
-func generateShortURL (link string) string {
-
-    sum := sha256.Sum256( []byte( link ) )
-    slice := sum[:]
-    encoded := base64.StdEncoding.EncodeToString(slice)
-
-    return encoded[:hashLength]
-}
 
 func getFullUrl( shortUrl string ) (string, int) {
 
@@ -50,98 +33,114 @@ func getFullUrl( shortUrl string ) (string, int) {
     return "", 404
 }
 
+func getLinkCnt( shortUrl string ) int {
+
+    val, ok := tableOfLinks[shortUrl]
+    if ok == true {
+        return val.Cnt
+    }
+    return -1
+}
+func generateShortURL (link string) string {
+
+    sum := sha256.Sum256( []byte( link ) )
+    slice := sum[:]
+    encoded := base64.StdEncoding.EncodeToString(slice)
+
+    return encoded[:hashLength]
+}
+func getLinkStatistiсs( w http.ResponseWriter, r *http.Request ) {
+
+    hash, err := getHashFromRequest( r )
+
+    if err == nil {
+        encHash, _ := url.QueryUnescape( hash )
+        cnt := getLinkCnt( encHash )
+
+        if cnt >= 0 {
+            str := fmt.Sprintf("Адрес %s посещали %d раз \n", baseUrl + encHash, cnt )
+            w.Write([]byte(str))
+
+        } else {
+            str := fmt.Sprintf("Адрес %s не существует в системе\n",
+                baseUrl + encHash )
+            w.Write([]byte(str))
+        }
+    } else {
+        fmt.Println(err)
+    }
+}
+func registerNewLink( w http.ResponseWriter, r *http.Request ) {
+
+    query, _ := url.QueryUnescape(r.URL.RawQuery)
+
+    if len(query) > 4 {
+        url := query[4:]
+        newStruct := fullUrl{ Url: url }
+        hash := generateShortURL(url)
+        tableOfLinks[hash] = newStruct
+
+        str := fmt.Sprintf("Короткая ссылка для %s - %s \n", url, baseUrl + hash)
+        w.Write([]byte(str))
+
+    } else {
+        w.Write([]byte("Введенный url неполный! \n"))
+    }
+}
+
+type getHashFromRequestError struct {
+    s string
+}
+
+func (e *getHashFromRequestError) Error() string {
+    return e.s
+}
+
 func getHashFromRequest( r *http.Request ) (string, error) {
 
-	if (len(r.URL.RawQuery) > hashLength) {
-		return r.URL.RawQuery[len(r.URL.RawQuery)- hashLength:], nil
+    if (len(r.URL.RawQuery) > hashLength) {
+        return r.URL.RawQuery[len(r.URL.RawQuery)- hashLength:], nil
 
-	} else {
-		err := getHashFromRequestError{s: "Невозможно получить короткую ссылку из запроса \n"}
-		return "", &err
-	}
+    } else {
+        err := getHashFromRequestError{s: "Невозможно получить короткую ссылку из запроса \n"}
+        return "", &err
+    }
 }
 
 func redirect( w http.ResponseWriter, r *http.Request ) {
 
-	link, err := getHashFromRequest( r )
-	if err == nil {
-		shortUrl, _ := url.QueryUnescape(link)
-		url, statusCode := getFullUrl( shortUrl )
+    link, err := getHashFromRequest( r )
+    if err == nil {
+        shortUrl, _ := url.QueryUnescape(link)
+        url, statusCode := getFullUrl( shortUrl )
 
-		if statusCode == 303 {
-			http.Redirect(w, r, url, http.StatusSeeOther)
+        if statusCode == 303 {
+            http.Redirect(w, r, url, http.StatusSeeOther)
 
-		} else {
-			http.Redirect(w, r, shortUrl, http.StatusNotFound)
-		}
-	} else {
-		fmt.Println(err)
-	}
+        } else {
+            http.Redirect(w, r, shortUrl, http.StatusNotFound)
+        }
+    } else {
+        fmt.Println(err)
+    }
 }
 
-func getLinkCnt( shortUrl string ) int {
 
-	val, ok := tableOfLinks[shortUrl]
-	if ok == true {
-		return val.Cnt
-	}
-	return -1
-}
-
-func getLinkStatistiсs( w http.ResponseWriter, r *http.Request ) {
-
-	hash, err := getHashFromRequest( r )
-
-	if err == nil {
-		encHash, _ := url.QueryUnescape( hash )
-		cnt := getLinkCnt( encHash )
-
-		if cnt >= 0 {
-			str := fmt.Sprintf("Адрес %s посещали %d раз \n", baseUrl + encHash, cnt )
-			w.Write([]byte(str))
-
-		} else {
-			str := fmt.Sprintf("Адрес %s не существует в системе\n",
-				baseUrl + encHash )
-			w.Write([]byte(str))
-		}
-	} else {
-		fmt.Println(err)
-	}
-}
-
-func registerNewLink( w http.ResponseWriter, r *http.Request ) {
-
-	query, _ := url.QueryUnescape(r.URL.RawQuery)
-
-	if len(query) > 8 {
-		url := query[4:]
-		newStruct := fullUrl{ Url: url }
-		hash := generateShortURL(url)
-		tableOfLinks[hash] = newStruct
-
-		str := fmt.Sprintf("Короткая ссылка для %s - %s \n", url, baseUrl + hash)
-		w.Write([]byte(str))
-
-	} else {
-		w.Write([]byte("Введенный url неполный! \n"))
-	}
-}
 
 func main () {
 
-	s := &http.Server{
-		Addr:           ":8080",
-	}
+    s := &http.Server{
+        Addr:           ":8080",
+    }
 
-	// обработчики запросов:
-	// - перейти по короткой ссылке
-	http.HandleFunc("/", redirect)
-	// - получить новую короткую ссылку
-	http.HandleFunc("/reg_new_link/", registerNewLink)
-	// - получить статистику переходов по короткой ссылке
-	http.HandleFunc("/get_link_statistiсs/", getLinkStatistiсs)
+    // обработчики запросов:
+    // - перейти по короткой ссылке
+    http.HandleFunc("/", redirect)
+    // - получить новую короткую ссылку
+    http.HandleFunc("/reg_new_link/", registerNewLink)
+    // - получить статистику переходов по короткой ссылке
+    http.HandleFunc("/get_link_statistiсs/", getLinkStatistiсs)
 
-	//запускаем сервер
-	s.ListenAndServe()
+    //запускаем сервер
+    s.ListenAndServe()
 }
--- servShortLink/servShortLink_test.go
+++ servShortLink/servShortLink_test.go
@@ -2,56 +2,54 @@ package main
 
 import
 (
-	"net/http"
-	"net/http/httptest"
-	"testing"
+    "net/http"
+    "net/http/httptest"
+    "testing"
     "io"
 )
 
 // проверяет перенаправление с короткого урла на длинный google.com
 func redirectGoogleCom( t *testing.T ) {
-	req, err := http.NewRequest("GET", "/" + "?Url=" + "http://localhost:8080?u=rGu2", nil)
+    req, err := http.NewRequest("GET", "/" + "?Url=" + "http://localhost:8080?u=rGu2", nil)
 
-	if err != nil {
-		t.Fatal(err)
-	}
+    if err != nil {
+        t.Fatal(err)
+    }
 
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(redirect)
+    rr := httptest.NewRecorder()
+    handler := http.HandlerFunc(redirect)
 
-	handler.ServeHTTP(rr, req)
+    handler.ServeHTTP(rr, req)
 
-	if status := rr.Code; status != http.StatusSeeOther {
-		t.Errorf("Возвращен неверный статус: получил %v ожидал %v",
-			status, http.StatusSeeOther)
-	}
+    if status := rr.Code; status != http.StatusSeeOther {
+        t.Errorf("Возвращен неверный статус: получил %v ожидал %v",
+            status, http.StatusSeeOther)
+    }
 }
 
-// проверяем счетчик посещений по короткой ссылке
 func getCntGoogleCom( t *testing.T, expected string ) {
-	req, err := http.NewRequest("GET", "/get_link_statistiсs/" + "?Url=" + "http://localhost:8080?u=rGu2", nil)
+    req, err := http.NewRequest("GET", "/get_link_statistiсs/" + "?Url=" + "http://localhost:8080?u=rGu2", nil)
 
-	if err != nil {
-		t.Fatal(err)
-	}
+    if err != nil {
+        t.Fatal(err)
+    }
 
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(getLinkStatistiсs)
+    rr := httptest.NewRecorder()
+    handler := http.HandlerFunc(getLinkStatistiсs)
 
-	handler.ServeHTTP(rr, req)
+    handler.ServeHTTP(rr, req)
 
-	body, err := io.ReadAll(rr.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	got := string(body)
+    body, err := io.ReadAll(rr.Body)
+    if err != nil {
+        t.Fatal(err)
+    }
+    got := string(body)
 
-	if expected != got {
-		t.Errorf("ожидал: %v получил: %v",
-			expected, got)
-	}
+    if expected != got {
+        t.Errorf("ожидал: %v получил: %v",
+            expected, got)
+    }
 }
-
 
 // проверяет:
 // - получение короткой ссылки для google.com
@@ -59,71 +57,71 @@ func getCntGoogleCom( t *testing.T, expected string ) {
 // - перенаправление с короткой ссылки на google.com
 // - состояние счетчика посещений после перенаправления (должно быть 1 посещение)
 func TestGoogleCom(t *testing.T) {
-	req, err := http.NewRequest("GET", "/reg_new_link/" + "?Url=" + "https://www.google.com", nil)
+    req, err := http.NewRequest("GET", "/reg_new_link/" + "?Url=" + "https://www.google.com", nil)
 
-	if err != nil {
-		t.Fatal(err)
-	}
+    if err != nil {
+        t.Fatal(err)
+    }
 
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(registerNewLink)
+    rr := httptest.NewRecorder()
+    handler := http.HandlerFunc(registerNewLink)
 
-	handler.ServeHTTP(rr, req)
+    handler.ServeHTTP(rr, req)
 
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("Возвращен неверный статус: получил %v ожидал %v",
-			status, http.StatusOK)
+    if status := rr.Code; status != http.StatusOK {
+        t.Errorf("Возвращен неверный статус: получил %v ожидал %v",
+            status, http.StatusOK)
 
-	} else {
-		body, err := io.ReadAll(rr.Body)
+    } else {
+        body, err := io.ReadAll(rr.Body)
 
-		if err != nil {
-			t.Fatal(err)
-		}
+        if err != nil {
+            t.Fatal(err)
+        }
 
-		expected:= "Короткая ссылка для https://www.google.com - http://localhost:8080?u=rGu2 \n"
-		got := string(body)
+        expected:= "Короткая ссылка для https://www.google.com - http://localhost:8080?u=rGu2 \n"
+        got := string(body)
 
-		if expected != got {
-			t.Errorf("ожидал: %v получил: %v",
-				expected, got)
-		}
+        if expected != got {
+            t.Errorf("ожидал: %v получил: %v",
+                expected, got)
+        }
 
-		getCntGoogleCom(t, "Адрес http://localhost:8080?u=rGu2 посещали 0 раз \n")
-		redirectGoogleCom(t)
-		getCntGoogleCom(t, "Адрес http://localhost:8080?u=rGu2 посещали 1 раз \n")
-	}
+        getCntGoogleCom(t, "Адрес http://localhost:8080?u=rGu2 посещали 0 раз \n")
+        redirectGoogleCom(t)
+        getCntGoogleCom(t, "Адрес http://localhost:8080?u=rGu2 посещали 1 раз \n")
+    }
 }
 
 // проверяет возвращаемую ошибку, есть дать пустой длинный урл
 func TestEmptyUrl(t *testing.T) {
-	req, err := http.NewRequest("GET", "/reg_new_link/", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+    req, err := http.NewRequest("GET", "/reg_new_link/", nil)
+    if err != nil {
+        t.Fatal(err)
+    }
 
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(registerNewLink)
+    rr := httptest.NewRecorder()
+    handler := http.HandlerFunc(registerNewLink)
 
-	handler.ServeHTTP(rr, req)
+    handler.ServeHTTP(rr, req)
 
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("Возвращен неверный статус: получил %v ожидал %v",
-			status, http.StatusOK)
+    if status := rr.Code; status != http.StatusOK {
+        t.Errorf("Возвращен неверный статус: получил %v ожидал %v",
+            status, http.StatusOK)
 
-	} else {
-		body, err := io.ReadAll(rr.Body)
+    } else {
+        body, err := io.ReadAll(rr.Body)
 
-		if err != nil {
-			t.Fatal(err)
-		}
+        if err != nil {
+            t.Fatal(err)
+        }
 
-		expected:= "Введенный url неполный! \n"
-		got := string(body)
+        expected:= "Введенный url неполный! \n"
+        got := string(body)
 
-		if expected != got {
-			t.Errorf("ожидал: %v получил: %v",
-				expected, got)
-		}
-	}
+        if expected != got {
+            t.Errorf("ожидал: %v получил: %v",
+                expected, got)
+        }
+    }
 }
